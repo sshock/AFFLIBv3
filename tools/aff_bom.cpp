ae1fe969f136f103d13f6bb946deef162c65ba6d
Bernard Spil <brnrd@FreeBSD.org>
2017-02-12 21:37:44 +0100

    Fix build with LibreSSL
      - Patch checks for OPENSSL_VERSION_NUMBER > 1.1

--- tools/aff_bom.cpp
+++ tools/aff_bom.cpp
@@ -36,7 +36,7 @@
 #endif
 
 /* Support OpenSSL before 1.1.0 */
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 #define EVP_MD_CTX_new EVP_MD_CTX_create
 #define EVP_MD_CTX_free EVP_MD_CTX_destroy
 #endif
